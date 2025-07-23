fb00444cc8154801737cc2fd858c6dccd048359b
Gabriel Leao <comercial.gabrielleao@gmail.com>
2025-07-22 22:37:09 -0300

    Primeiro commit

---
+++ Assets/Scripts/moveLex.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveLex : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float InputX = Input.GetAxisRaw ("Horizontal");
+		float InputY = Input.GetAxisRaw ("Vertical");
+
+		if (InputX != 0 || InputY != 0) {
+
+			anim.SetBool ("Run", true);
+
+		} 
+
+		else {
+
+			anim.SetBool ("Run", false);
+
+		}
+
+		if (InputX > 0 && InputY == 0) {
+
+			transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.Euler (0, 90, 0), 5 * Time.deltaTime);
+
+		}
+
+		if (InputX < 0 && InputY == 0) {
+
+			transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.Euler (0, -90, 0), 5 * Time.deltaTime);
+
+		}
+
+		if (InputY > 0) {
+
+			transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.Euler (0, 0, 0), 5 * Time.deltaTime);
+
+			if (InputX > 0) {
+
+				transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.Euler (0, 45, 0), 5 * Time.deltaTime);
+
+			}
+
+			if (InputX < 0) {
+
+				transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.Euler (0, -45, 0), 5 * Time.deltaTime);
+
+			}
+
+		}
+
+		if (InputY < 0) {
+
+			transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.Euler (0, 180, 0), 5 * Time.deltaTime);
+
+			if (InputX > 0) {
+
+				transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.Euler (0, -225, 0), 5 * Time.deltaTime);
+
+			}
+
+			if (InputX < 0) {
+
+				transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.Euler (0, 225, 0), 5 * Time.deltaTime);
+
+			}
+
+		}
+		
+	}
+}
---
+++ Assets/Scripts/rotateObj.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateObj : MonoBehaviour {
+
+	public float rotationSpeed = 0.5f;
+
+	float inicialHeigh, minHeigh, maxHeigh;
+	public float velocidadeFlutuar = 5f;
+	public float offsetConstant;
+	float floatingPos;
+	public float lambda, xLambda;
+	bool trocaMove;
+
+	// Use this for initialization
+	void Start () {
+
+		inicialHeigh = transform.position.y;
+		minHeigh = inicialHeigh - offsetConstant;
+		maxHeigh = inicialHeigh + offsetConstant;
+		lambda = 0.5f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//Debug.Log (lambda);
+
+		transform.Rotate (0, 0, rotationSpeed * Time.deltaTime);
+
+		lambda = Mathf.Clamp (lambda, 0, 1);
+		xLambda = Mathf.Lerp (minHeigh, maxHeigh, lambda);
+
+		if (trocaMove) {
+
+			lambda = lambda + velocidadeFlutuar * Time.deltaTime;
+
+		} 
+
+		else {
+
+			lambda = lambda - velocidadeFlutuar * Time.deltaTime;
+
+		}
+
+		if (lambda >= 1) {
+
+			lambda = 0.99f;
+			trocaMove = !trocaMove;
+
+		} 
+
+		if (lambda <= 0) {
+
+			lambda = 0.11f;
+			trocaMove = !trocaMove;
+
+		}
+
+		transform.position = new Vector3 (transform.position.x, xLambda, transform.position.z);
+		
+	}
+}
